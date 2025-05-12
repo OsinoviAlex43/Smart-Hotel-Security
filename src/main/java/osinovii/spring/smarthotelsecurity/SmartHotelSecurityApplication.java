4d9870f2dddaf950d898eedc0ce844cf58894923
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-05-12 23:52:30 +0300

    Create project

---
+++ src/main/java/osinovii/spring/smarthotelsecurity/SmartHotelSecurityApplication.java
@@ -0,0 +1,13 @@
+package osinovii.spring.smarthotelsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHotelSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHotelSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/osinovii/spring/smarthotelsecurity/SmartHotelSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package osinovii.spring.smarthotelsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHotelSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
